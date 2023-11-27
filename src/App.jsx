72a89318df313adc66d9a0558bf2767aefc70700
shakir.kamal <shakir.kamal@kendimed.com>
2023-11-27 21:06:32 +0200

    Created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignnUp from "./pages/SignnUp";
 const App = () => {
-  return <div className="bg-red-800">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignnUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
