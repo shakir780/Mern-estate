f6905f4855b386850d0bef0d3dd66690fd10d1fc
shakir.kamal <shakir.kamal@kendimed.com>
2023-12-05 14:25:35 +0200

    create listing api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import userRouter from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -23,6 +24,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Serval Error";
